80b87422e159635f697d256f0a0d0525a291a92a
brenda-wanjiku <brendawanjiku@live.com>
2020-02-23 15:47:11 +0300

    Deletes pop up function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
-$(document).ready(function () {
-    $(".designBlock").click(function () {
-        $("#design").toggle()
-        $(".clickable").toggle()
+function myFunction() {
+        }
 
-    });
+    $(document).ready(function () {
+        $(".designBlock").click(function () {
+            $("#design").toggle()
+            $(".clickable").toggle()
 
-    $(".developmentBlock").click(function () {
-        $("#development").toggle()
-        $(".clickable").toggle()
+        });
 
-    });
+        $(".developmentBlock").click(function () {
+            $("#development").toggle()
+            $(".clickable").toggle()
 
-    $(".productBlock").click(function () {
-        $("#product").toggle()
-        $(".clickable").toggle()
+        });
 
-    });
-    $(".img1").hover(function () {
-        $("#work1").fadeToggle("slow")
-    });
-    $("#work1").fadeToggle(0.5)
-});
+        $(".productBlock").click(function () {
+            $("#product").toggle()
+            $(".clickable").toggle()
+
+        });
+        $(".img1").hover(function () {
+            $("#work1").fadeIn("slow")
+        });
+        $("#work1").fadeOut(3000)
+    });       
 
     /* $(".img1").click(function(){
-         $(".img1").fadeIn ()
-     });*/
-    /*$(function(){ 
-        $(".img1").hover(function () {
-            $(this).find("#work1").fadeIn();
+     $(".img1").fadeIn ()
+ });*/
+/*$(function(){
+    $(".img1").hover(function () {
+        $(this).find("#work1").fadeIn();
 });
 });*/
-    
-       /* $("#work1").hover (function() {
-            $("#work1").fadeOut(0.5);
-        });*/
+
+/* $("#work1").hover (function() {
+     $("#work1").fadeOut(0.5);
+ });*/
