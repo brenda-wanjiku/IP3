79cbf441c4cfeb45455234ccc3dce8581e71a4c3
brenda-wanjiku <brendawanjiku@live.com>
2020-02-24 11:22:01 +0300

    Edits Js script

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
-function myFunction() {
+$(document).ready(function () {
+    $(".designBlock").click(function () {
+        $("#design").toggle()
+        $(".clickable").toggle()
+
+    });
+
+    $(".developmentBlock").click(function () {
+        $("#development").toggle()
+        $(".clickable").toggle()
+
+    });
+
+    $(".productBlock").click(function () {
+        $("#product").toggle()
+        $(".clickable").toggle()
+
+    });
+    /* $(".img1").hover(function () {
+       $("#work1").fadeIn("slow")
+   });
+   $("#work1").fadeOut(3000)
+  });*/
+
+    $("form #contactUs").submit(function (event) {
+        let name = $("#inputName").val();
+        let email = $("#inputEmail").val();
+        let message = $("#textMessage").val();
+        var inputDetails = ["inputName", "inputEmail", "textMessage"]
+
+        if (name === "" || email === "" || message === "") {
+            return alert("Enter details");
         }
-
-    $(document).ready(function () {
-        $(".designBlock").click(function () {
-            $("#design").toggle()
-            $(".clickable").toggle()
-
-        });
-
-        $(".developmentBlock").click(function () {
-            $("#development").toggle()
-            $(".clickable").toggle()
-
-        });
-
-        $(".productBlock").click(function () {
-            $("#product").toggle()
-            $(".clickable").toggle()
-
-        });
-        $(".img1").hover(function () {
-            $("#work1").fadeIn("slow")
-        });
-        $("#work1").fadeOut(3000)
-    });       
-
-    /* $(".img1").click(function(){
-     $(".img1").fadeIn ()
- });*/
-/*$(function(){
-    $(".img1").hover(function () {
-        $(this).find("#work1").fadeIn();
+        
+        inputDetails.forEach(function (detail) {
+                if (detail === "inputName") {
+                    alert("Thank you " + name + " we have received your message")
+                };
+            });
+    });
 });
-});*/
-
-/* $("#work1").hover (function() {
-     $("#work1").fadeOut(0.5);
- });*/
